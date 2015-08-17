fe0ea2c8d972b59cfa3f85555700d7e8f9ae71cb
push.default <petrauskas.mykolas@gmail.com>
2015-08-17 20:15:04 +0000

    added Profile New action

--- app/controllers/profiles_controller.rb
+++ app/controllers/profiles_controller.rb
@@ -1,3 +1,8 @@
 class ProfilesController < ApplicationController
-    
+    def new
+      # form where a user can fill out her own profile]
+      @user = User.find( params[:user_id] )
+      @profile = @user.build_profile
+      
+  end
 end
